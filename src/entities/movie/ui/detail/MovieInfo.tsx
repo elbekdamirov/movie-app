bfa64c3ee57f4084dd888f49cdb7dc6665374cb8
elbekdamirov <elbekmain@gmail.com>
2025-09-26 17:34:20 +0500

    fix

--- src/entities/movie/ui/detail/MovieInfo.tsx
+++ src/entities/movie/ui/detail/MovieInfo.tsx
@@ -1,90 +1,72 @@
-import { memo } from "react";
-import { FaInstagram, FaFacebook, FaYoutube } from "react-icons/fa";
-import { MdOutlineLocalMovies, MdTheaters } from "react-icons/md";
-import { TbMicrophone2 } from "react-icons/tb";
-import { GiBasketballBall } from "react-icons/gi";
-import { RiAdvertisementFill } from "react-icons/ri";
-import { FaQuestionCircle } from "react-icons/fa";
-import { MdContactPhone } from "react-icons/md";
-import { IoDocumentTextOutline } from "react-icons/io5";
+import { memo, type FC } from "react";
+import { useMovie } from "../../model/useMovie";
+import { createImageUrl } from "@/shared/utils";
+import { Image } from "antd";
+import { Title } from "@/shared/ui/title/Title";
+import { Link } from "react-router-dom";
 
-import playmarket from "@/shared/assets/play.png";
-import footerlogo from "@/shared/assets/logo.svg";
-import appstore from "@/shared/assets/appstore.svg";
+interface Props {
+  id: string;
+}
 
-export const Footer = memo(() => {
+export const MovieInfo: FC<Props> = memo(({ id }) => {
+  const { getMovieById, getMovieInfo } = useMovie();
+  const { data } = getMovieById(id);
+  const { data: imageData } = getMovieInfo(id, "images");
+
   return (
-    <footer className=" text-gray-300 py-10 bg-black">
-      <div className="container mx-auto  rounded-xl px-6 py-10 grid grid-cols-2 sm:justify-center sm:grid-cols-2 md:grid-cols-4 gap-8 bg-[#111111]">
-        <div className="space-y-4">
-          <img src={footerlogo} alt="footerlogo" className="w-24" />
-          <div className="flex flex-col gap-3">
-            <img
-              src={playmarket}
-              alt="Google Play"
-              className="w-36 cursor-pointer hover:opacity-80 transition"
+    <div>
+      <section>
+        {data?.backdrop_path && (
+          <img
+            src={createImageUrl(data.backdrop_path)}
+            alt={data.title}
+            className="w-full max-h-[600px] object-contain"
+          />
+        )}
+      </section>
+
+      <section className="container py-6 space-y-3">
+        <h1 className="text-3xl font-bold">{data?.title}</h1>
+
+        <p className="text-lg text-black dark:text-white">
+          Budget: {data?.budget ? `${data.budget.toLocaleString()} USD` : "N/A"}
+        </p>
+
+        {data?.homepage && (
+          <a
+            href={data.homepage}
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            Official Website
+          </a>
+        )}
+      </section>
+
+      <section className="container flex overflow-x-auto gap-2 pb-4">
+        {imageData?.backdrops
+          ?.slice(0, 20)
+          .map((item: { file_path: string }, inx: number) => (
+            <Image
+              key={inx}
+              className="min-w-[200px] rounded"
+              src={createImageUrl(item.file_path)}
+              alt={`${data?.title} backdrop ${inx + 1}`}
+              preview={true}
             />
-            <img
-              src={appstore}
-              alt="App Store"
-              className="w-36 cursor-pointer hover:opacity-80 transition"
-            />
-          </div>
-        </div>
+          ))}
+      </section>
 
-        <div>
-          <h3 className="text-red-500 font-semibold mb-4">About Us</h3>
-          <ul className="space-y-3">
-            <li className="flex items-center text-red-700 gap-2 hover:text-red-500 cursor-pointer">
-              <IoDocumentTextOutline /> Public Offer
-            </li>
-            <li className="flex items-center text-red-700 gap-2 hover:text-red-500 cursor-pointer">
-              <RiAdvertisementFill /> Advertising
-            </li>
-            <li className="flex items-center text-red-700 gap-2 hover:text-red-500 cursor-pointer">
-              <FaQuestionCircle /> F.A.Q
-            </li>
-            <li className="flex items-center text-red-700 gap-2 hover:text-red-500 cursor-pointer">
-              <MdContactPhone /> Contacts
-            </li>
-          </ul>
+      <section className="container mt-10">
+        <Title>Tabs</Title>
+        <div className="flex gap-4">
+          <Link to={""}>Review</Link>
+          <Link to={"cast"}>Cast</Link>
+          <Link to={"others"}>Others</Link>
         </div>
-
-        <div>
-          <h3 className="text-red-500 font-semibold mb-4">Categories</h3>
-          <ul className="space-y-3">
-            <li className="flex items-center text-red-700 gap-2 hover:text-red-500 cursor-pointer">
-              <MdOutlineLocalMovies /> Movies
-            </li>
-            <li className="flex items-center text-red-700 gap-2 hover:text-red-500 cursor-pointer">
-              <MdTheaters /> Theater
-            </li>
-            <li className="flex items-center text-red-700 gap-2 hover:text-red-500 cursor-pointer">
-              <TbMicrophone2 /> Concerts
-            </li>
-            <li className="flex items-center text-red-700 gap-2 hover:text-red-500 cursor-pointer">
-              <GiBasketballBall /> Sports
-            </li>
-          </ul>
-        </div>
-
-        <div>
-          <h3 className="text-red-500 font-semibold mb-4">Contact Us</h3>
-          <p className="text-red-500 font-semibold mb-4">+998 (91) 012-20-77</p>
-          <h3 className="text-red-500 font-semibold mb-3">Follow Us</h3>
-          <div className="flex text-red-500 space-x-4 text-2xl">
-            <a href="#" className="hover:text-red-500">
-              <FaInstagram />
-            </a>
-            <a href="#" className="hover:text-red-500">
-              <FaFacebook />
-            </a>
-            <a href="#" className="hover:text-red-500">
-              <FaYoutube />
-            </a>
-          </div>
-        </div>
-      </div>
-    </footer>
+      </section>
+    </div>
   );
 });
